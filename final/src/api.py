6148cfa880e7a0a12991bba67d0221e89b2d6a22
Long Vu <longvu@utexas.edu>
2021-05-07 23:35:35 -0500

    fixes

--- final/src/api.py
+++ final/src/api.py
@@ -27,9 +27,12 @@ def get_data():
     return data
 
 
+def delete_database():
+    rd_raw.flushall()
+
 @app.route('/data/load', methods=['GET'])
 def load_data():
-    rd_raw.flushall()  # Deletes all data on the redis db to start on clean slate.
+    delete_database() # Deletes all data on the redis db to start on clean slate.
 
     with open("Austin_Affordable_Housing.json", "r") as f:
         housing_data = json.load(f)
@@ -95,6 +98,7 @@ def download(jid):
 # CRUD Operations ======================================================================================================
 
 # CREATE - Add a new property
+# curl localhost:5035/data/add_house -X POST -H "Content-Type: application/json" -d '{"Address": "5812 Berkman Dr","Zip Code": 78723, "Unit Type": "Single Family", "Tenure": "", "City Amount": 0, "Longitude": -97.692, "Latitude": 30.292107, "Property Manager Phone Number": "", "Property Manager Email": ""}'
 @app.route('/data/add_house', methods=['POST'])
 def add_house():
     housing_data = get_data()
@@ -111,7 +115,7 @@ def add_house():
     new_manager_number = new_house['Property Manager Phone Number']
     new_manager_email = new_house['Property Manager Email']
 
-    housing_data['Housing Data'].append({"Project ID": new_project_id, "Address": new_address, "Zip Code": new_zip,
+    housing_data.append({"Project ID": new_project_id, "Address": new_address, "Zip Code": new_zip,
                                          "Unit Type": new_unit_type, "Tenure": new_tenure,
                                          "City Amount": new_city_amount, \
                                          "Longitude": new_longitude, "Latitude": new_latitude, \
@@ -127,9 +131,16 @@ def add_house():
 @app.route('/data/get/<Project_ID>', methods=['GET'])
 def get_house(Project_ID):
     housing_data = get_data()
+    output = []
 
-    json_list = housing_data['Housing Data']
-    output = [x for x in json_list if x['Project ID'] == Project_ID]
+    for a in housing_data:
+        currID = a['Project ID']
+
+        if currID == Project_ID:
+            output = a
+
+    # output = [x for x in json_list if x['Project ID'] == Project_ID]
+
     return jsonify(output)
 
 
@@ -150,17 +161,17 @@ def update_house(Project_ID):
     new_manager_number = new_house['Property Manager Phone Number']
     new_manager_email = new_house['Property Manager Email']
 
-    list = [x for (x, i) in enumerate(housing_data['Housing Data']) if i['Project ID'] == Project_ID]
+    list = [x for (x, i) in enumerate(housing_data) if i['Project ID'] == Project_ID]
 
-    housing_data['Housing Data'][list[0]]['Address'] = new_address
-    housing_data['Housing Data'][list[0]]['Zip Code'] = new_zip
-    housing_data['Housing Data'][list[0]]['Unit Type'] = new_unit_type
-    housing_data['Housing Data'][list[0]]['Tenure'] = new_tenure
-    housing_data['Housing Data'][list[0]]['City Amount'] = new_city_amount
-    housing_data['Housing Data'][list[0]]['Longitude'] = new_longitude
-    housing_data['Housing Data'][list[0]]['Latitude'] = new_latitude
-    housing_data['Housing Data'][list[0]]['Property Manager Phone Number'] = new_manager_number
-    housing_data['Housing Data'][list[0]]['Property Manager Email'] = new_manager_email
+    housing_data[list[0]]['Address'] = new_address
+    housing_data[list[0]]['Zip Code'] = new_zip
+    housing_data[list[0]]['Unit Type'] = new_unit_type
+    housing_data[list[0]]['Tenure'] = new_tenure
+    housing_data[list[0]]['City Amount'] = new_city_amount
+    housing_data[list[0]]['Longitude'] = new_longitude
+    housing_data[list[0]]['Latitude'] = new_latitude
+    housing_data[list[0]]['Property Manager Phone Number'] = new_manager_number
+    housing_data[list[0]]['Property Manager Email'] = new_manager_email
 
     rd_raw.set('Housing Data', json.dumps(housing_data, indent=2))
 
@@ -172,9 +183,17 @@ def update_house(Project_ID):
 def delete_house(Project_ID):
     housing_data = get_data()
 
-    list = [x for (x, i) in enumerate(housing_data['Housing Data']) if i['Project ID'] == Project_ID]
+    for a in housing_data:
+        currID = a['Project ID']
 
-    housing_data['Housing Data'].pop(list[0])
+        if currID == Project_ID:
+            print(a)
+            housing_data.remove(a)
+
+    # list = [x for (x, i) in enumerate(housing_data) if i['Project ID'] == Project_ID]
+    # housing_data.pop(list[0])
+
+    delete_database()
     rd_raw.set('Housing Data', json.dumps(housing_data, indent=2))
 
     return "Property has been deleted"
